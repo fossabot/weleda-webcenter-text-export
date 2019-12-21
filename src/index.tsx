cf106f836e4ba924e614088e4106f1be07e36342
Manuele Vaccari <manuele.vaccari@gmail.com>
2019-12-21 14:58:12 +0100

    Fix unnecessary import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CssBaseline from "@material-ui/core";
 import {ThemeProvider} from '@material-ui/core/styles';
 import App from './components/App';
 import theme from './theme';

7585ba39d5322c30de53b76396e431e81d5bec50
D3strukt0r <dev@d3strukt0r.me>
2021-10-04 15:17:28 +0200

    Update react

--- pwa/src/components/App/App.test.tsx
+++ pwa/src/components/App/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders drop field', () => {
-  const { getByText } = render(<App />);
-  const textElement = getByText(/Click or drag the XML file to this area to upload./i);
+  render(<App />);
+  const textElement = screen.getByText(/Click or drag the XML file to this area to upload./i);
   expect(textElement).toBeInTheDocument();
 });
--- pwa/src/index.tsx
+++ pwa/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {ThemeProvider} from '@material-ui/core/styles';
+import './index.css';
 import App from './components/App';
 import theme from './theme';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <ThemeProvider theme={theme}>
@@ -15,5 +16,10 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

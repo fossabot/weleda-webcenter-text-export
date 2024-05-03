2aa89443dbd5a4222e59342317ca83abdadb4ef0
D3strukt0r <dev@d3strukt0r.me>
2024-05-03 17:33:38 +0200

    Format file

--- pwa/src/hooks/useDarkMode.tsx
+++ pwa/src/hooks/useDarkMode.tsx
@@ -1,44 +1,44 @@
 import { useEffect, useState } from 'react';
 
 const useLocalStorage = (key: string, initialValue?: any) => {
-    const [storedValue, setStorageValue] = useState(() => {
-        try {
-            const item = window.localStorage.getItem(key);
-            return item ? JSON.parse(item) : initialValue;
-        } catch (error) {
-            console.log(error);
-            return initialValue;
-        }
-    });
+  const [storedValue, setStorageValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
 
-    const setValue = (value: any) => {
-        try {
-            const valueToStore = value instanceof Function ? value(storedValue) : value;
+  const setValue = (value: any) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
 
-            setStorageValue(valueToStore);
+      setStorageValue(valueToStore);
 
-            window.localStorage.setItem(key, JSON.stringify(valueToStore));
-        } catch (error) {
-            console.log(error);
-        }
-    };
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return [storedValue, setValue];
+  return [storedValue, setValue];
 };
 
 const useDarkMode = () => {
-    const [enabled, setEnabled] = useLocalStorage('dark-theme');
-    // const isEnabled = typeof enabledState === 'undefined' && enabled;
-    const isEnabled = enabled;
+  const [enabled, setEnabled] = useLocalStorage('dark-theme');
+  // const isEnabled = typeof enabledState === 'undefined' && enabled;
+  const isEnabled = enabled;
 
-    useEffect(() => {
-        const className = 'dark';
-        const bodyClass = window.document.body.classList;
+  useEffect(() => {
+    const className = 'dark';
+    const bodyClass = window.document.body.classList;
 
-        isEnabled ? bodyClass.add(className) : bodyClass.remove(className);
-    }, [enabled, isEnabled]);
+    isEnabled ? bodyClass.add(className) : bodyClass.remove(className);
+  }, [enabled, isEnabled]);
 
-    return [enabled, setEnabled];
+  return [enabled, setEnabled];
 };
 
 export default useDarkMode;
